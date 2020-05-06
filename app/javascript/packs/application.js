4205adc1f1b33e965a34990573a9b9107021c4ef
hausmanne <elie.hausmann@gmail.com>
2020-05-06 23:09:19 +0200

    Displaying room messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
