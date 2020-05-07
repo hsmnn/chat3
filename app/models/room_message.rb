4080755ee7bd44dddcff5ec7bfe4c9df7d1b6b0c
hausmanne <elie.hausmann@gmail.com>
2020-05-07 22:45:04 +0200

    Subscribe to the room stream

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,8 +1,8 @@
 class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
   belongs_to :user
-  
+
   def as_json(options)
     super(options).merge(user_avatar_url: user.gravatar_url)
-  end
+  ende are going to add some data in the room page in order to use them via Javascript to subscribe to the appropriate stre
 end
