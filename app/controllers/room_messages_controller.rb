fa9645bbbd5d0570c00259a8bc1583ed669d6df1
hausmanne <elie.hausmann@gmail.com>
2020-05-07 22:38:45 +0200

    Broadcast room messages

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -3,6 +3,7 @@ class RoomMessagesController < ApplicationController
 
     def create
         @room_message = RoomMessage.create user:current_user, room: @room, message: params.dig(:room_message, :message)
+        RoomChannel.broadcast_to @room, @room_message
     end
 
     protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true, presence: true
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
